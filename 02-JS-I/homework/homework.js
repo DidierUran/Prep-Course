c1e5ef5918979c9bf3d3c4945b74e4f9d253bed3
didierj <didierj.uran@gmail.com>
2022-09-27 23:59:55 -0500

    tarea completada ahora si

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -240,9 +240,9 @@ function esVocal(letra){
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
-  }
+  }else{
   return "Dato incorrecto"
-
+  }
 }
 
 
